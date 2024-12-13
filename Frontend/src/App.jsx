e09505537be08c967db3c1ee8df91b7eac5c271e
Charanjeet Singh <cjeet336@gmail.com>
2024-12-13 13:24:42 +0400

    Added Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element = {<Doctors />} />
+        <Route path= "/login" element={<Login />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointment />} />
+        <Route path='/my-appointments/:docId' element={<Appointment />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   )
 }
