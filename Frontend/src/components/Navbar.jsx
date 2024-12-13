15383eb3a5e5949517b47e4470a8e307a7b83809
Charanjeet Singh <cjeet336@gmail.com>
2024-12-13 14:55:28 +0400

    Completed Navigation

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const navigate = useNavigate()
     const [showMenu, setShowMenu] = useState(false)
     const [token, setToken] = useState(true)
-    
+
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
@@ -30,10 +30,21 @@ const Navbar = () => {
             </NavLink>
         </ul>
         <div className='flex items-center gap-4'>
-            <button onClick = {()=> navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+            {
+                token ? <div className='flex items-center cursor-pointer gap-2 group relative '>
+                    <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+                    <img className='w-2.5'src={assets.dropdown_icon} alt="" />
+                    <div className='absolute top-0 right-0 pt-14 text-gray-600 z-20 hidden group-hover:block font-medium text-base'> 
+                        <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                            <p onClick={()=> navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                            <p onClick={()=> navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointment</p>
+                            <p onClick = {()=>setToken(false)}className='hover:text-black cursor-pointer'>Logout</p>
+                        </div>
+                    </div>
+                </div> : <button onClick = {()=> navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer'>Create Account</button>
+            }  
         </div>
     </div>
   )
 }
-
 export default Navbar
