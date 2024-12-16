d28fcaaf619561da9f3514547f00761faff8423d
Charanjeet Singh <cjeet336@gmail.com>
2024-12-16 11:04:02 +0400

    added javascript function onClick to scroll on top

--- Frontend/src/components/SpecialityMenu.jsx
+++ Frontend/src/components/SpecialityMenu.jsx
@@ -15,7 +15,7 @@ function SpecialityMenu() {
       <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
         {specialityData.map((item, index) => {
           return (
-            <Link className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[10px] transition-all duration-500 mb-5" key={index} to={`/doctors/${item.speciality}`}>
+            <Link onClick = {()=>scrollTo(0,0)} className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[10px] transition-all duration-500 mb-5" key={index} to={`/doctors/${item.speciality}`}>
               <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
               <p>{item.speciality}</p>
             </Link>
