a3571b82c988b13c9238c783d3cda38d94822ce9
Charanjeet Singh <cjeet336@gmail.com>
2024-12-23 11:15:31 +0400

    Related Doctors page created and added in info doctors

--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
+import RelatedDoctors from "./RelatedDoctors";
 
 const Appointment = () => {
   const { docId } = useParams();
@@ -163,6 +164,8 @@ const Appointment = () => {
             Book an appointment
           </button>
         </div>
+        {/* Listing Related Doctors */}
+        <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
       </div>
     )
   );
