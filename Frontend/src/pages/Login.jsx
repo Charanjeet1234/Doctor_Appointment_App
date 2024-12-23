e0a19c785a195722a42d19471eb1045345b07365
Charanjeet Singh <cjeet336@gmail.com>
2024-12-23 12:22:23 +0400

    Login and Sign Up page created

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-function Login() {
+const Login = () => {
+  const [state, setState] = useState("Sign Up");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <form
+      className="min-h-[80vh] flex items-center"
+      action="
+    "
+    >
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 rounded-rl text-zinc-600 text-sm shadow-lg">
+        <p className="text-2xl font-semibold">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </p>
+        <p>
+          Please {state === "Sign Up" ? "Sign up" : "Log in"} to book
+          Appointment
+        </p>
+        {state === "Sign Up" && (
+          <div className="w-full">
+            <p>Full Name</p>
+            <input
+              className="border border-zinc-300 rounded w-full pt-2 mt-1"
+              type="text"
+              onChange={(e) => setName(e.target.name)}
+              value={name}
+              required
+            />
+          </div>
+        )}
 
-export default Login
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            className="border border-zinc-300 rounded w-full pt-2 mt-1"
+            type="email"
+            onChange={(e) => setName(e.target.email)}
+            value={email}
+            required
+          />
+        </div>
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className="border border-zinc-300 rounded w-full pt-2 mt-1"
+            type="password"
+            onChange={(e) => setName(e.target.password)}
+            value={password}
+            required
+          />
+        </div>
+        <button className="bg-primary text-white w-full px-8 py-3">
+          {state === "Sign Up" ? "Create Account" : "LogIn"}
+        </button>
+        {state === "Sign Up" ? (
+          <p>
+            Already hava an account ?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className="text-primary underline cursor-pointer"
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create an new account?{" "}
+            <span
+              onClick={() => setState("Sign Up")}
+              className="text-primary underline cursor-pointer"
+            >
+              Click here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default Login;
