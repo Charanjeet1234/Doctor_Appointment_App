f41630791c658a45267d93eba4fc3325d9f4db12
Charanjeet Singh <cjeet336@gmail.com>
2025-01-14 15:03:17 +0400

    ADDED MONGODB AND CLOUDINARY FOR IMAGES

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config'
-
+import connectDB from "./config/mongodb.js";
 
 // app config
 
 const app = express();
 // define port
 const port = process.env.PORT || 4000;
-
+connectDB()
 // middlewares 
 app.use(express.json());
 // it allows to connect frontend to backend server
