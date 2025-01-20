6da133b338192f0fe54306305d057533dab6dd66
Charanjeet Singh <cjeet336@gmail.com>
2025-01-20 14:32:49 +0400

    Added doctor API

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,18 +1,100 @@
-
-
+import validator from "validator";
+import bcrypt from "bcrypt";
+import doctorModel from "../models/doctorModel.js";
+import { v2 as cloudinary } from "cloudinary";
 // Api for adding doctor
-const addDoctor = async (req,res) =>
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+
+    } = req.body;
+    const imageFile = req.file;
+      console.log({imageFile, name, email, password, speciality, degree, experience, about, fees, address})
+
+    // Checking for all data to add doctor
+
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address
+    )
+      return res.status(400).json({ success: false, message: "All fields are required" });
+
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Please enter valid email" });
+    }
+
+    // validate strong password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Password must be at least 8 characters long",
+      });
+    }
+
+    // hasing doctor password
+    const salt = await bcrypt.genSalt(10) ; //Number of rounds to encrypt the password
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // upload image to the cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type:"image",
+    });
+
+    const imageUrl = imageUpload.secure_url; 
+
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+
+    const newDoctor = new doctorModel(doctorData);
+    // save doctor to the database
+    await newDoctor.save();
+    res.json({ success: true, message: "Doctor added successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// API for the admin login
+const loginAdmin = async (req, res) =>
 {
-    try
-    {
-      const {name, email, password, speciality, degree, experience, about, fees, address } = req.body
-      const imageFile = req.file
-    //   console.log({imageFile,name, email, password, speciality, degree, experience, about, fees, address})
+    try {
 
     }
-    catch (error) {
-
+    catch (error)
+    {
+        console.error(error);
+        res.status(500).json({ success: false, message: error.message });
     }
 }
+export { addDoctor, loginAdmin };
 
-export {addDoctor};
+
+
--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,6 @@ app.use(cors());
 app.use('/api/admin', adminRouter)
 // localhost:4000/api/admin/add-doctor
 
-
-
-
 app.get("/", (req, res) => {
     res.send("Hello from the server ! API is working");
 })
