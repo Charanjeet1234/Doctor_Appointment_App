28437e6724b6847cfdcf77733cd3d7eea7f2781a
Charanjeet Singh <cjeet336@gmail.com>
2025-01-20 15:58:53 +0400

    Added css

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -3,19 +3,23 @@ import { assets } from '../assets/assets'
 const Login = () => {
     const [state, setState] = useState('Admin')
   return (
-    <form>
-    <div>
-        <p><span>{state}</span>Login</p>
-        <div>
+    <form className='min-h-[80vh] flex items-center'>
+    <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5e5e5e] text-sm shadow-lg '>
+        <p className='text-2xl font-semibold m-auto'><span className='text-primary'>{state}</span> Login</p>
+        <div className='w-full'>
             <p>Email</p>
-            <input type="email" required />
+            <input className='border borde-[#dadada] rounded w-full p-2 mt-1' type="email" required />
         </div>
-        <div>
+        <div className='w-full'>
             <p>Password</p>
-            <input type="password" required />
+            <input className='border border-[#dadada] rounded w-full p-2 mt-1' type="password" required />
         </div>
-        <button>Login</button>
+        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>Login</button>
+          {
+            state === 'Admin' ? <p>Doctor Login?<span className='underline cursor-pointer text-primary' onClick={()=>setState('Doctor')}> Click here</span></p> : <p>Admin Login? <span className='underline cursor-pointer text-primary' onClick={()=>setState('Admin')}> Click here</span></p>
+          }
     </div>
+
     </form>
   )
 }
