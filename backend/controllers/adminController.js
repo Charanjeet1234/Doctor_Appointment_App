ccaac2473ceb61fbea069be203cfcb24eed21062
Charanjeet Singh <cjeet336@gmail.com>
2025-01-20 14:54:07 +0400

    Created Admin Login Api and middleware

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import doctorModel from "../models/doctorModel.js";
 import { v2 as cloudinary } from "cloudinary";
+import jwt from 'jsonwebtoken'
 // Api for adding doctor
 const addDoctor = async (req, res) => {
   try {
@@ -86,7 +87,17 @@ const addDoctor = async (req, res) => {
 const loginAdmin = async (req, res) =>
 {
     try {
-
+        const {email, password} = req.body
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD)
+        {
+            const token = jwt.sign(email+password, process.env.JWT_SECRET )
+            res.json({ success: true, message: "Admin logged in successfully", token });
+           
+        }
+        else 
+        {
+            res.json({ success: false, message: "Invalid email or password" });  
+        }
     }
     catch (error)
     {
