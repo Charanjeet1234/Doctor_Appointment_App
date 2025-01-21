8558c5b89059cb3eccc2d422a66cd507130d9042
Charanjeet Singh <cjeet336@gmail.com>
2025-01-21 13:00:42 +0400

    Logout functionality completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 import Login from './pages/login'
 import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
 const App = () => {
   const {aToken} = useContext(AdminContext)
 
   return aToken ? (
-    <div>
+    <div className='bg-[#f8f9fd]'>
       <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
