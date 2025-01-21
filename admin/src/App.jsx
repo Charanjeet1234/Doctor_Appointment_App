d5272e2b902722412013ee7154c0a8987720215f
Charanjeet Singh <cjeet336@gmail.com>
2025-01-21 13:08:20 +0400

    Routes of admin pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,10 @@ import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 const App = () => {
   const {aToken} = useContext(AdminContext)
 
@@ -13,7 +17,13 @@ const App = () => {
       <Navbar />
      <div>
       <Sidebar />
-      
+      <Routes>
+        <Route path ="/" element={<></>} />
+        <Route path ="/admin-dashboard" element={< Dashboard/>} />
+        <Route path ="/all-appointments" element={< AllAppointments/>} />
+        <Route path ="/add-doctor" element={< AddDoctor/>} />
+        <Route path ="/doctor-list" element={< DoctorsList/>} />
+      </Routes>
      </div>
     </div>
   ) : (
